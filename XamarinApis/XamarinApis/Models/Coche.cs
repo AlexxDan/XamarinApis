085479e80081ad3fe9185fd5dc9743910f2c4243
Alejandro Navaroli <alejandronavaroli@gmail.com>
2021-05-28 13:27:21 +0200

    Add project files.

---
+++ XamarinApis/XamarinApis/Models/Coche.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApis.Models
+{
+   public class Coche
+    {
+        [JsonProperty("idcoche")]
+        public int IdCoche { get; set; }
+
+        [JsonProperty("marca")]
+        public String Marca { get; set; }
+
+        [JsonProperty("modelo")]
+        public String Modelo { get; set; }
+
+        [JsonProperty("conductor")]
+        public String Conductor { get; set; }
+
+        [JsonProperty("imagen")]
+        public String Imagen { get; set; }
+    }
+}
